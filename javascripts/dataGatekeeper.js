fefdde15811ea67c551c2905eb72d6546c713137
Tom Dugan <thomas@aadwebb.com>
2018-04-14 12:38:57 -0500

    did stuff

--- javascripts/dataGatekeeper.js
+++ javascripts/dataGatekeeper.js
@@ -1,9 +1,11 @@
 const loadDepartments = require('./departments');
 const departmentDom = require('./departmentDom');
+const data = require('./data');
 
 const departmentSuccess = function () {
-  const data = JSON.parse(this.responseText).departments;
-  departmentDom(data);
+  const departmentsData = JSON.parse(this.responseText).departments;
+  data.setDepartments(departmentsData);
+  departmentDom(departmentsData);
 };
 
 const departmentMalfunction = function () {
